c87529bcd24974c7cf59c22b089caf401641c227
Giacomo Accardi Neto <44924062+giaccneto@users.noreply.github.com>
2025-03-28 13:50:58 -0400

    Update UsuarioRepository.java

--- Seletivo-SEPLAG/PROJETO-PR-TICO-IMPLEMENTA-O-BACK-END/Seletivo-SEPLAG/src/main/java/com/giaccneto/Seletivo_SEPLAG/infraestructure/repository/UsuarioRepository.java
+++ Seletivo-SEPLAG/PROJETO-PR-TICO-IMPLEMENTA-O-BACK-END/Seletivo-SEPLAG/src/main/java/com/giaccneto/Seletivo_SEPLAG/infraestructure/repository/UsuarioRepository.java
@@ -4,4 +4,7 @@ import com.giaccneto.Seletivo_SEPLAG.infraestructure.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+    boolean existsByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 }
